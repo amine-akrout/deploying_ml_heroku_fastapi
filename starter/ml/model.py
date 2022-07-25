262489c96a198f2d548b1d862c6d94563d7b652a
Amine AKROUT <aakrout@creativeadvtech.com>
2022-07-25 19:42:44 +0200

    train model

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,7 +1,9 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
+
+
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
@@ -18,7 +20,8 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    rf_classifier = RandomForestClassifier()
+    return rf_classifier
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +60,4 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
--- starter/train_model.py
+++ starter/train_model.py
@@ -1,11 +1,14 @@
 # Script to train machine learning model.
-
+import pickle
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from ml.model import *
+from ml.data import process_data
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
-
+data = pd.read_csv("../data/census.csv", sep=", ")
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
@@ -26,3 +29,11 @@ X_train, y_train, encoder, lb = process_data(
 # Proces the test data with the process_data function.
 
 # Train and save a model.
+trained_model = train_model(X_train, y_train)
+
+with open("../model/trained_model.pkl", "wb",) as file:
+    pickle.dump(trained_model, file)
+with open("../model/lb.pkl", "wb",) as file:
+    pickle.dump(lb, file)
+with open("../model/encoder.pkl", "wb",) as file:
+    pickle.dump(encoder, file)
